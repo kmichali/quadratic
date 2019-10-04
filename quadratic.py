55342908a2e37f41028ddd5b9f456a9c0b333204
Katerina Michalickova <kmichali@imperial.ac.uk>
2019-10-04 15:12:32 +0100

    a change

--- quadratic.py
+++ quadratic.py
@@ -19,12 +19,10 @@ if d < 0:
 if d > 0 :
     rd = math.sqrt(d)
     x1 = (-b + rd)/(2*a)
-    x2 = (-b - rd)/(2*a)
+     x2 = (-b - rd)/(2*a)
     print("two solutions :" , x1, x2) 
 elif d == 0:
     x = -b/(2*a)
     print( "one solution :" , x) 
 else:
     print("something is wrong, we shoudn't  be here!")
-
-
